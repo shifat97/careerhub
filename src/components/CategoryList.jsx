93aca2e75d7405b0787ec0a3f4eb5e536edd5265
shifat97 <shifatbinreza2@gmail.com>
2024-05-21 14:35:14 +0600

    added banner and category

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,7 +1,33 @@
+import {useEffect, useState} from "react";
+
 export default function CategoryList() {
+    const [categoryData, setCategoryData] = useState([]);
+
+    useEffect(() => {
+        fetch("categories.json")
+            .then((res) => res.json())
+            .then((data) => setCategoryData(data));
+    }, []);
+
     return (
-        <div>
-            <h1>This is category list.</h1>
+        <div className="mt-10">
+            <h1 className="text-4xl text-center font-bold">Job Category List</h1>
+            <p className="text-center text-gray-600 my-4">Explore thousands of job opportunities with all the information you need. Its your future</p>
+            <div className="flex justify-between gap-4">
+                {
+                    categoryData.map((category) => (
+                        <div key={category.id}>
+                            <div>
+                                <img src={category.logo} alt=""/>
+                            </div>
+                            <div>
+                                <h3 className="font-bold">{category.category_name}</h3>
+                                <p>{category.availability}</p>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     )
 }
